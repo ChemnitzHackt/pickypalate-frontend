0586eac84806a52a8166c891b5a0d549ed0c06cc
alanfermann <anna.lanfermann@posteo.de>
2017-10-28 19:56:13 +0200

    formatted entries

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -26,14 +26,24 @@ const DetailView = function(props) {
                     </dl>
                 </address>
                 <time className="opening-times">
-                    <strong>Opening Hours:</strong>&nbsp;Samstag:&nbsp;12:00–19:00
+                    <dl>
+                        <dt><strong>Opening Hours:</strong></dt>
+                        <dd>{props.data["opening_hours"] || "No information"}</dd>
+                    </dl>
                 </time>
-                <p className="telephone-number">
-                    <strong>Telephone Number:</strong>&nbsp;0341 30698857
-                </p>
-                <p className="description">
+                <div className="telephone-number">
+                    <dl>
+                        <dt><strong>Telephone Number:</strong></dt>
+                        <dd>{props.data["phone"] || "No information"}</dd>
+                    </dl>
+                </div>
+                <div className="description">
                     Das im Shabby Chic gestaltete Café bietet zu Kaffee und Frappés feine amerikanische Süßwaren und Eis am Stil.
-                </p>
+                </div>
+                <div className="furtherInformation">
+
+                    { props.data.website && <a href={props.data.website}>Website</a> }
+                </div>
             </main>
             <footer>
 
